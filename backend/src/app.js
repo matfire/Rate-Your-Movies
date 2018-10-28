b8d5f567f919b53d978d659ae6e9595e6fea5195
unknown <mgassend@gmail.com>
2018-10-28 11:30:24 +0100

    added data

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,38 +1,114 @@
 import {GraphQLServer} from 'graphql-yoga';
+import axios from 'axios'
+const Sequelize = require("sequelize")
 
-const movies = [{
-    title:"test",
-    id:1
-},
-{
-    title:"test2",
-    id:2
-}]
+const sequelize = new Sequelize("movie_api", "root", "", {
+	host: "localhost",
+	dialect: "mysql",
+	operatorAliases: false,
+
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
+})
+sequelize.authenticate().then(() =>{console.log("success")})
+
+const Movie = sequelize.define('movie', {
+	title: {
+		type: Sequelize.STRING
+	},
+	imdbID: {
+		type: Sequelize.STRING
+	},
+	rating: {
+		type: Sequelize.FLOAT
+	},
+	votes: {
+		type: Sequelize.FLOAT
+    },
+    directorId: {
+        type: Sequelize.INTEGER
+    }
+})
+
+const Director = sequelize.define('director', {
+    name: {
+        type: Sequelize.STRING
+    },
+
+})
 
 const typeDefs = `
     type Query {
-        movie(id: ID): Movie!
         movies: [Movie]
+        movie(id: ID, title: String): [Movie]
+        director(name: String): Director
+        directors: [Director]
+        top10: [Movie]
     }
     type Movie {
         title: String
         genre: String
         rating: Float
+        votes: Float
+        imdbID: String
         id: ID!
+        director: Director
+    }
+    type Director {
+        name: String
+        movies: [Movie]
     }
 `
 
 const resolvers = {
     Query: {
-        movie(parent, args, ctx, info) {
-            console.log(args)
-            return movies.filter((instance) => {
-                if (instance.id == args.id)
-                    return instance
-            })
+        movies(){
+            return Movie.findAll()
         },
-        movies() {
-            return movies
+        movie(parent, args, ctx){
+            if (args.id) {
+                return Movie.findAll({where:{id: args.id}})
+            }
+            if (args.title) {
+                return Movie.findAll({
+                    where: {
+                        title: {
+                            [Sequelize.Op.like]: args.title
+                        }
+                    }
+                })
+            }
+        },
+        directors(){
+            return Director.findAll()
+        },
+        top10(){
+            return Movie.findAll({
+                limit: 10,
+                order: sequelize.literal('rating DESC')
+            })
+        }
+    },
+    Movie: {
+        director(parent, args, ctx){
+            return Director.findOne({
+                where: {
+                    id: parent.directorId
+                }
+            })
+        }
+    },
+    Director: {
+        movies(parent, args, ctx){
+            return Movie.findAll({
+                where: {
+                    directorId: parent.id
+                }
+            })
         }
     }
 }
