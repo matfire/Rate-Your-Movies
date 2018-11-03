0b4ac1b3209399dc87a522675d66a1b50e98e0c1
Matteo <mgassend@gmail.com>
2018-11-03 12:34:53 +0100

    added reviews

--- src/containers/DetailedView.jsx
+++ src/containers/DetailedView.jsx
@@ -7,7 +7,8 @@ import { Card, CardText, CardBody,
 
 class DetailedView extends React.Component {
 	state = {
-		details: []
+		details: [],
+		reviews: []
 	}
 	componentDidMount() {
 		axios.get("https://api.themoviedb.org/3/movie/" + this.props.match.params.id + "?api_key=2005b3a7fc676c3bd69383469a281eff&language=en-US").then(res => {
@@ -15,22 +16,50 @@ class DetailedView extends React.Component {
 				details:res.data
 			})
 		})
+		axios.get("https://api.themoviedb.org/3/movie/" + this.props.match.params.id +"/reviews?api_key=2005b3a7fc676c3bd69383469a281eff&language=en-US").then(res => {
+			this.setState({
+				reviews: res.data.results
+			})
+		})
+	}
+
+	renderReviews = () => {
+		return(
+			this.state.reviews.map((r) => (
+				<React.Fragment>
+				<div className="row mt-3" key={r.id}>
+					<div className="col-sm-2">
+						<p>{r.author}</p>
+					</div>
+					<div className="col-sm-10">
+						<p>{r.content}</p>
+					</div>
+				</div>
+				<hr></hr>
+				</React.Fragment>
+		)))
 	}
 	render() {
+		const reviews = this.renderReviews()
 		return(
-			<div className="row">
-				<div className="col-sm-4 mt-5">
-					<img src={"https://image.tmdb.org/t/p/w342"+ this.state.details.poster_path} alt={this.state.details.original_title}/>
-				</div>
-				<div className="col-sm-8 mt-5">
-					<Card>
-						<CardBody>
-							<CardTitle>{this.state.details.original_title}</CardTitle>
-							<CardText>
+			<div>
+				<div className="row">
+					<div className="col-sm-4 mt-5">
+						<img src={"https://image.tmdb.org/t/p/w342"+ this.state.details.poster_path} alt={this.state.details.original_title}/>
+					</div>
+					<div className="col-sm-8 mt-5">
+						<Card>
+							<CardBody>
+								<CardTitle>{this.state.details.original_title}</CardTitle>
+								<CardText>
 
-							</CardText>
-						</CardBody>
-					</Card>
+								</CardText>
+							</CardBody>
+						</Card>
+					</div>
+				</div>
+				<div id="reviews" className="mt-5">
+				{reviews}
 				</div>
 			</div>
 		)
