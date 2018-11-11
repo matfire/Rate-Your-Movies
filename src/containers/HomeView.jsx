19ab349069776319c854813017a901eed98cdda9
unknown <mgassend@gmail.com>
2018-11-11 21:06:36 +0100

    added theater's slider on homepage

--- src/containers/HomeView.jsx
+++ src/containers/HomeView.jsx
@@ -17,8 +17,8 @@ class HomeView extends React.Component {
 		movie_playing: []
 	}
 
-	createMovieSliderComponents = () => {
-		const results = this.state.movie_trending.map((movie) => (
+	createMovieSliderComponents = (data) => {
+		const results = data.map((movie) => (
 			<div className="movie-item" style={{width:"224px"}}>
 				<div className="mv-img">
 					<a href={"/movies/" + movie.id}>
@@ -35,26 +35,56 @@ class HomeView extends React.Component {
 		))
 		return results
 	}
+	createPlayingMovieSliderComponents = () => {
+		const res = this.state.movie_playing.map((movie) => (
+				<div className="slide-it slick-slide slick-cloned" style={{width: "192px"}}>
+					<div className="movie-item">
+						<div className="mv-img">
+							<img src={"https://image.tmdb.org/t/p/w342" + movie.poster_path} alt={movie.title} widht={185} height={284} />
+						</div>
+						<div class="hvr-inner">
+					  	<a href={"/movies/" + movie.id}> Read more <i className="ion-android-arrow-dropright"></i> </a>
+					  </div>
+						<div className="title-in">
+							<h6><a href={"/movies/" + movie.id}>{movie.title}</a></h6>
+							<p><i className="ion-android-star"></i><span>{movie.vote_average}</span>/10</p>
+						</div>
+					</div>
+				</div>
+		))
+		return res
+	}
 	componentDidMount() {
 		axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=2005b3a7fc676c3bd69383469a281eff").then(res => {
 			this.setState({movie_trending:res.data.results})
 		})
+		axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=2005b3a7fc676c3bd69383469a281eff&language=en-US&page=1").then(res => {
+			this.setState({movie_playing:res.data.results})
+		})
 	}
 	render() {
-		const TrendingMovieSliderItems = this.createMovieSliderComponents()
+		const TrendingMovieSliderItems = this.createMovieSliderComponents(this.state.movie_trending)
+		const PlayingMovieSliderItems = this.createPlayingMovieSliderComponents(this.state.movie_playing)
 		return(
 			<React.Fragment>
 				<div className="slider movie-items">
 					<div className="container">
-						<AliceCarousel items={TrendingMovieSliderItems} duration={1000} responsive={{0:{items:1}, 1024:{items:3}}} autoPlay={true} buttonsDisabled={true} mouseDragEnabled />
+						<AliceCarousel items={TrendingMovieSliderItems} duration={1000} responsive={{0:{items:1}, 1024:{items:3}}} autoPlay={true} buttonsDisabled={true} mouseDragEnabled stagePadding={{paddingLeft:0, paddingRight:0}}/>
 					</div>
 				</div>
 				<div className="movie-items">
 					<div className="container">
 						<div className="row ipad-width">
 							<div className="col-md-8">
-								<div className="title-hd">
-									<h2>IN THEATER</h2>
+								<div className="title-hd"><h2>IN THEATERS NOW</h2></div>
+								<div className="tabs">
+									<div className="tab-content">
+										<div className="tab-active">
+											<div className="row">
+												<AliceCarousel items={PlayingMovieSliderItems} autoPlay={false} buttonsDisabled={true} mouseDragEnabled responsive={{0:{items:1}, 1024:{items:4}}} />
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 							<div className="col-md-4"></div>
